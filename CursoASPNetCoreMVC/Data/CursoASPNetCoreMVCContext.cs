91deb5bb63ac00ec25bce180bb0d54d3fe79b1cf
Robson Silva <rms2307@outlook.com>
2021-08-02 15:55:12 -0300

    Added: CRUD Department

---
+++ CursoASPNetCoreMVC/Data/CursoASPNetCoreMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CursoASPNetCoreMVC.Models;
+
+namespace CursoASPNetCoreMVC.Data
+{
+    public class CursoASPNetCoreMVCContext : DbContext
+    {
+        public CursoASPNetCoreMVCContext (DbContextOptions<CursoASPNetCoreMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CursoASPNetCoreMVC.Models.Department> Department { get; set; }
+    }
+}
--- CursoASPNetCoreMVC/Startup.cs
+++ CursoASPNetCoreMVC/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CursoASPNetCoreMVC.Data;
 
 namespace CursoASPNetCoreMVC
 {
@@ -33,6 +35,9 @@ namespace CursoASPNetCoreMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<CursoASPNetCoreMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CursoASPNetCoreMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
